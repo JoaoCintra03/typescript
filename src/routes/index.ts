773f7d7d4125ee1cf5d1a7077a16efaa7d29871d
JoaoCintra03 <161538925+JoaoCintra03@users.noreply.github.com>
2025-05-29 22:06:35 -0300

    Add files via upload

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+
+import categoria from './categorias'
+import usuario from './usuarios'
+
+const routes = Router()
+
+routes.use('/categorias', categoria)
+
+routes.use('/usuarios', usuario)
+
+
+
+export default routes
