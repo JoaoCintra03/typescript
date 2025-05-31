497b5ce514d0a2c2a950215553f9c649d7b69f37
JoaoCintra03 <161538925+JoaoCintra03@users.noreply.github.com>
2025-05-30 22:05:10 -0300

    Add files via upload

--- usuarios.ts
+++ usuarios.ts
@@ -37,67 +37,15 @@ router.post("/", async (req,res) => {
         })
         return
 })
+ router.put('/:id', async (req, res) =>{
 
-router.put("/:id", async (req, res) => {
-  const updateBodySchema = z.object({
-    nome: z.string().optional(),
-    email: z.string().email().optional(),
-    senha: z.string().min(6).optional(),
-  });
+  const registerParamsSchema = z.object({
+    id: z.string().min(1)
+  })
 
-  try {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      return res.status(400).json({ message: "ID inválido" });
-    }
+  const id = +registerParamsSchema.parse(req.params).id
+  
 
-    const dadosAtualizados = updateBodySchema.parse(req.body);
-
-    if (dadosAtualizados.senha) {
-      dadosAtualizados.senha = await hash(dadosAtualizados.senha, 8);
-    }
-
-    const usuarioExistente = await knex("usuarios").where({ id }).first();
-
-    if (!usuarioExistente) {
-      return res.status(404).json({ message: "Usuário não encontrado" });
-    }
-
-    await knex("usuarios").where({ id }).update(dadosAtualizados);
-
-    const usuarioAtualizado = await knex("usuarios").where({ id }).first();
-
-    return res.status(200).json({
-      message: "Usuário atualizado com sucesso",
-      usuario: usuarioAtualizado,
-    });
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return res.status(400).json({ errors: error.errors });
-    }
-    return res.status(500).json({ message: "Erro interno do servidor" });
-  }
 })
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      return res.status(400).json({ message: "ID inválido" });
-    }
-
-    const usuarioExistente = await knex("usuarios").where({ id }).first();
-
-    if (!usuarioExistente) {
-      return res.status(404).json({ message: "Usuário não encontrado" });
-    }
-
-    await knex("usuarios").where({ id }).del();
-
-    return res.status(200).json({ message: "Usuário deletado com sucesso" });
-  } catch {
-    return res.status(500).json({ message: "Erro interno do servidor" });
-  }
-});
 
 export default router
